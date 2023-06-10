a18d2460614846dae213d395f9036db5264abf1d
Nathova <122864131+Nathova@users.noreply.github.com>
2023-06-10 12:21:10 +0300

    Описание основного и базовых классов и функций

---
+++ classes/head_classes.py
@@ -0,0 +1,125 @@
+from abc import ABC, abstractmethod
+from dotenv import load_dotenv
+import os
+import requests
+import json
+
+
+class Head(ABC):
+    @abstractmethod
+    def get_request(self):
+        pass
+
+    @staticmethod
+    def get_connector(file_name):
+        """ Возвращает экземпляр класса Connector """
+        pass
+
+
+class HH(Head):
+    """Класс для доступа к API HeadHunter"""
+    vacancies_all = []
+    vacancies = []
+    vacancies_dicts = []
+
+    def __init__(self, vacancy):
+        self.vacancy = vacancy
+
+    def get_request(self):
+        """Метод для отправки запроса на HeadHunter. Осуществляет проверки,
+        записывает полученную информацию в .json файл,
+        возвращает словари для последующей работы с ними.
+        """
+
+        for num in range(
+                50):  # при значении 50 выбирает из 1000 вакансий те в которых есть информация о З/П и она в RUR
+            url = 'https://api.hh.ru/vacancies'
+            params = {'text': {self.vacancy}, "areas": 113, 'per_page': 20, 'page': num}
+            response = requests.get(url, params=params)
+            info = response.json()
+            if info is None:
+                return "Данные не получены"
+            elif 'errors' in info:
+                return info['errors'][0]['value']
+            elif 'items' not in info:
+                return "Нет вакансий"
+            else:
+                for vacancy in range(20):
+                    self.vacancies_all.append(
+                        vacancy)  # добавлено для проверки количества полученных вакансий до отбора
+                    if info['items'][vacancy]['salary'] is not None \
+                            and info['items'][vacancy]['salary']['currency'] == "RUR":
+                        self.vacancies.append([info['items'][vacancy]['employer']['name'],
+                                               info['items'][vacancy]['name'],
+                                               info['items'][vacancy]['apply_alternate_url'],
+                                               info['items'][vacancy]['snippet']['requirement'],
+                                               info['items'][vacancy]['salary']['from'],
+                                               info['items'][vacancy]['salary']['to']])
+        for vacancy in self.vacancies:
+            vacancy_dict = {'employer': vacancy[0], 'name': vacancy[1], 'url': vacancy[2], 'requirement': vacancy[3],
+                            'salary_from': vacancy[4], 'salary_to': vacancy[5]}
+            if vacancy_dict['salary_from'] is None:
+                vacancy_dict['salary_from'] = 0
+            elif vacancy_dict['salary_to'] is None:
+                vacancy_dict['salary_to'] = vacancy_dict['salary_from']
+            self.vacancies_dicts.append(vacancy_dict)
+
+        with open(f'{self.vacancy}_hh.json', 'w', encoding='UTF-8') as file:
+            json.dump(self.vacancies_dicts, file, indent=2, ensure_ascii=False)
+        print(f"Отбор осуществляется из {len(self.vacancies_all)} вакансий (проверка обращения к сервису)")
+        return self.vacancies_dicts
+
+
+class SuperJob(Head):
+    """Метод для отправки запроса на SuperJob осуществляет проверки,
+    записывает полученную информацию в .json файл,
+    возвращает словари для последующей работы с ними.
+    """
+    vacancies = []
+    vacancies_dicts = []
+
+    def __init__(self, vacancy):
+        self.vacancy = vacancy
+        load_dotenv()
+        self.api_key = os.getenv('SJ_API_KEY', 'key_error')
+
+    def get_request(self):
+        url = 'https://api.superjob.ru/2.0/vacancies/'
+        headers = {
+            'X-Api-App-Id': os.getenv('SJ_API_KEY'),
+        }
+
+        params = {
+            'keywords': f'{self.vacancy}',
+            'per_page': 100,
+            'area': 113,
+            'page': 0}
+
+        while len(self.vacancies) <= 50:  # указание необходимого количества вакансий в поиске
+            response = requests.get(url, headers=headers, params=params)
+            response_decode = response.content.decode()
+            response.close()
+            data = json.loads(response_decode)
+            vacancies = data['objects']
+            for vacancy in vacancies:
+                if vacancy['payment_from'] != 0 and vacancy['payment_to'] != 0 and vacancy['currency'] == 'rub':
+                    try:
+                        self.vacancies.append(
+                            [vacancy['client']['title'], vacancy['profession'],
+                             vacancy['link'], vacancy['candidat'],
+                             vacancy['payment_from'], vacancy['payment_to']])
+                    except KeyError:
+                        continue
+            params['page'] += 1
+        for vacancy in self.vacancies:
+            vacancy_dict = {'employer': vacancy[0], 'name': vacancy[1], 'url': vacancy[2], 'requirement': vacancy[3],
+                            'salary_from': vacancy[4], 'salary_to': vacancy[5]}
+            if vacancy_dict['salary_to'] == 0:
+                vacancy_dict['salary_to'] = vacancy_dict['salary_from']
+            self.vacancies_dicts.append(vacancy_dict)
+        with open(f'{self.vacancy}_sj.json', 'w', encoding='UTF-8') as file:
+            json.dump(self.vacancies_dicts, file, indent=2, ensure_ascii=False)
+
+        return self.vacancies_dicts
+
+
---
+++ utils/main.py
@@ -0,0 +1,60 @@
+from classes.connector import Connector
+from classes.job import Vacancy, sorting, get_top
+
+"""Добрый день, Уважаемый Пользователь! С помощью данного сервиса можно подбирать и сортировать вакансии по нужным параметрам."""
+def main():
+    user_input = input("""На каком ресурсе хотите осуществить поиск:
+HeadHunter: нажмите 1
+SuperJob: нажмите 2
+Выход: нажмите 0\n""").lower()
+    while user_input not in ["1", "2", "0"]:
+        print("Введите: 1 (для HeadHunter), 2 (для SuperJob), 0 (для выхода)")
+        user_input = input()
+    if user_input == "0":
+        print("Программа закончила работу")
+        quit()
+    user_prof = input("""Введите желаемую специальность 
+(выдает только те специальности, у которых заработная плата в рублях)\n""").lower()
+    vacancies = None
+    con = Connector(user_prof)
+    if user_input == "1":
+        try:
+            con.connectHH()
+        except IndexError:
+            print("Cпециальность не найдена")
+            quit()
+        vacancies = con.select_HH()
+    elif user_input == "2":
+        try:
+            con.connectSJ()
+        except IndexError:
+            print("Cпециальность не найдена")
+            quit()
+        vacancies = con.select_SJ()
+    else:
+        print("Благодарим, что воспользовались нашим сервисом")
+        quit()
+    print(f"Всего найдено вакансий {len(vacancies)}")
+    for vacancy in vacancies:
+        vacancy_class = Vacancy(vacancy['employer'], vacancy['name'], vacancy['url'], vacancy['requirement'],
+                                vacancy['salary_from'], vacancy['salary_to'])
+        print(vacancy_class)
+    user_sort = input("\nХотите отсортировать вакансии по зарплате Y/N? ").lower()
+    if user_sort == "y":
+        for sort in sorting(vacancies):
+            print(sort)
+        try:
+            user_top = int(input("""\nХотите вывести список топ вакансий? 
+(Введите количество вакансий) """))
+            if user_top:
+                for top in get_top(vacancies, user_top):
+                    print(top)
+        except ValueError:
+            print("Благодарим, что воспользовались нашим сервисом")
+    else:
+        print("Благодарим, что воспользовались нашим сервисом")
+
+
+if __name__ == "__main__":
+    main()
+
